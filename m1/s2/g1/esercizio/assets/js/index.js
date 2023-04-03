bd4bc5368342caaf8795d2feb7bf749bd91ff158
Giafal <gianluca.falcone@yahoo.it>
2023-04-03 16:36:56 +0200

    esercizio s2 g1

--- m1/s2/g1/esercizio/assets/js/index.js
+++ m1/s2/g1/esercizio/assets/js/index.js
@@ -1,2 +1,30 @@
-alert('Usa il pop up per dare dei messaggi');
-window.alert('Sto studiando JS');
+// alert("Usa il pop up per dare dei messaggi");
+// window.alert("Sto studiando JS");
+
+var nome = "Gianluca";
+console.log(nome);
+
+document.getElementById("nome").innerHTML = nome;
+
+console.log("Hello world!");
+
+// document.write("Ciao mondo!");
+document.write("<br>Io sono Gianluca");
+
+document.getElementById("saluto").innerText = "Ciao a tutti";
+document.getElementById("contenuto").textContent =
+  "<strong>Questo è un contenuto</strong>";
+document.getElementById("nome-strong").innerHTML = "<strong>nome</strong>";
+
+var eta = prompt("Quanti hanni hai?");
+
+if (eta >= 18 && eta <= 120) {
+  document.getElementById("eta").innerHTML = "Sei maggiorenne, puoi entrare";
+  //   location.href = "https://www.youtube.com/";
+} else if (eta > 120) {
+  document.getElementById("eta").innerHTML = "Sei un bugiardo";
+  //   location.href = "https://www.museoegizio.it/";
+} else {
+  document.getElementById("eta").innerHTML = "Sei minorenne, non puoi entrare";
+  //   location.href = "https://www.raiplay.it/dirette/raiyoyo";
+}
--- m1/s2/g1/esercizio/assets/js/naming.js
+++ m1/s2/g1/esercizio/assets/js/naming.js
@@ -3,14 +3,26 @@
 
 /*var pet = 'gatto';
 var Pet = 'cane';*/
-var petPreferito = 'criceto';
-var PetPreferito = 'coniglio';
+// var petPreferito = "criceto";
+// var PetPreferito = "coniglio";
 
-var $pet = 'giraffa';
-console.log($pet);
-var _pet = 'leone';
-console.log(_pet);
-var _pet2 = 'pantera';
-document.write(_pet2);
+// var $pet = "giraffa";
+// console.log($pet);
+// var _pet = "leone";
+// console.log(_pet);
+// var _pet2 = "pantera";
+// document.write(_pet2);
 
 //var 12 = 'numero';
+
+var firstName = "Gianluca";
+console.log(firstName);
+var lastName = "Falcone";
+console.log(lastName);
+var age = 35;
+console.log(age);
+var $money = "pochi";
+console.log($money);
+var _favouritePet = "cane";
+console.log(_favouritePet);
+document.write("<br>Ciao belli");
