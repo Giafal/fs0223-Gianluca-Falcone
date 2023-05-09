3bffdc0815130064f9355bbe7e4939fda01a3288
Giafal <gianluca.falcone@yahoo.it>
2023-05-09 17:16:47 +0200

    fix

--- m2/s3/g2/esercizio/script2.js
+++ m2/s3/g2/esercizio/script2.js
@@ -1,33 +1,3 @@
-// const timerDisplay = document.getElementById("timer");
-
-// let hours = 0;
-// let minutes = 0;
-// let seconds = 0;
-
-// setInterval(updateTimer, 1000);
-
-// function updateTimer() {
-//   seconds++;
-
-//   if (seconds === 60) {
-//     minutes++;
-//     seconds = 0;
-//   }
-
-//   if (minutes === 60) {
-//     hours++;
-//     minutes = 0;
-//   }
-
-//   const timeString = `${hours.toString().padStart(2, "0")}:${minutes
-//     .toString()
-//     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-
-//   timerDisplay.textContent = timeString;
-
-//   sessionStorage.setItem("timer", timeString);
-// }
-
 let seconds = sessionStorage.getItem("timer") || 0;
 
 const timerElement = document.getElementById("timer2");
