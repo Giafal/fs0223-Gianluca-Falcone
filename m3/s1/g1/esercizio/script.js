4b5abee60fba52f987e0797ee727c3bd8293e012
Giafal <gianluca.falcone@yahoo.it>
2023-05-22 15:12:14 +0200

    fix

--- m3/s1/g1/esercizio/script.js
+++ m3/s1/g1/esercizio/script.js
@@ -1,17 +1,21 @@
-console.log("ciao");
 var player1 = 40;
 var player2 = 60;
 var playerNumbers = function (a, b) {
     var randomNumber = Math.floor(Math.random() * 100) + 1;
     console.log(randomNumber);
-    if (a == randomNumber || b == randomNumber) {
-        console.log("Uno dei due giocatori ha indovinato il numero casuale");
+    if (a == randomNumber) {
+        console.log("Il primo giocatore ha indovinato il numero casuale");
     }
-    else if (Math.abs(a - randomNumber) < Math.abs(b - randomNumber)) {
-        console.log("Il primo giocatore si è avvicinato di più");
+    else if (b == randomNumber) {
+        console.log("Il secondo giocatore ha indovinato il numero casuale");
     }
-    else if (Math.abs(a - randomNumber) > Math.abs(b - randomNumber)) {
-        console.log("Il secondo giocatore si è avvicinato di più");
+    else {
+        if (Math.abs(a - randomNumber) < Math.abs(b - randomNumber)) {
+            console.log("Nessuno dei due ha indovinato il numero, ma il primo giocatore si è avvicinato di più");
+        }
+        else if (Math.abs(a - randomNumber) > Math.abs(b - randomNumber)) {
+            console.log("Nessuno dei due ha indovinato il numero, ma il secondo giocatore si è avvicinato di più");
+        }
     }
 };
 playerNumbers(player1, player2);
--- m3/s1/g1/esercizio/script.ts
+++ m3/s1/g1/esercizio/script.ts
@@ -1,17 +1,23 @@
-console.log("ciao");
-
 let player1: number = 40;
 let player2: number = 60;
 
 const playerNumbers = (a: number, b: number): void => {
   let randomNumber: number = Math.floor(Math.random() * 100) + 1;
   console.log(randomNumber);
-  if (a == randomNumber || b == randomNumber) {
-    console.log("Uno dei due giocatori ha indovinato il numero casuale");
-  } else if (Math.abs(a - randomNumber) < Math.abs(b - randomNumber)) {
-    console.log("Il primo giocatore si è avvicinato di più");
-  } else if (Math.abs(a - randomNumber) > Math.abs(b - randomNumber)) {
-    console.log("Il secondo giocatore si è avvicinato di più");
+  if (a == randomNumber) {
+    console.log("Il primo giocatore ha indovinato il numero casuale");
+  } else if (b == randomNumber) {
+    console.log("Il secondo giocatore ha indovinato il numero casuale");
+  } else {
+    if (Math.abs(a - randomNumber) < Math.abs(b - randomNumber)) {
+      console.log(
+        "Nessuno dei due ha indovinato il numero, ma il primo giocatore si è avvicinato di più"
+      );
+    } else if (Math.abs(a - randomNumber) > Math.abs(b - randomNumber)) {
+      console.log(
+        "Nessuno dei due ha indovinato il numero, ma il secondo giocatore si è avvicinato di più"
+      );
+    }
   }
 };
 
