11b18effa3ae9d8250687625385ad3a8cef816ce
Giafal <gianluca.falcone@yahoo.it>
2023-05-22 15:00:35 +0200

    esercizio g1

---
+++ m3/s1/g1/esercizio/script.ts
@@ -0,0 +1,18 @@
+console.log("ciao");
+
+let player1: number = 40;
+let player2: number = 60;
+
+const playerNumbers = (a: number, b: number): void => {
+  let randomNumber: number = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber);
+  if (a == randomNumber || b == randomNumber) {
+    console.log("Uno dei due giocatori ha indovinato il numero casuale");
+  } else if (Math.abs(a - randomNumber) < Math.abs(b - randomNumber)) {
+    console.log("Il primo giocatore si è avvicinato di più");
+  } else if (Math.abs(a - randomNumber) > Math.abs(b - randomNumber)) {
+    console.log("Il secondo giocatore si è avvicinato di più");
+  }
+};
+
+playerNumbers(player1, player2);
